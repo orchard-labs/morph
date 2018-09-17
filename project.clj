8614355e599efbb77f236e063a6c33acda5fd3f4
J Irving <j@lollyshouse.ca>
2018-09-17 17:55:59 -0400

    Bump for next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ca.orchard-labs/morph "1.1.2"
+(defproject ca.orchard-labs/morph "1.1.3-SNAPSHOT"
   :description "A small collection of useful transformations"
   :url "http://github.com/orchard-labs/morph"
   :license {:name "Eclipse Public License"
